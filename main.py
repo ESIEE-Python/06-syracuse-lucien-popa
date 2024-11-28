50ead2e79689fedd39bf2a7fb25bfd7251b7d03f
Lucien-Popa <lucien.popa@edu.esiee.fr>
2024-11-28 09:16:16 +0000

    Exercice Syracuse Lucien Popa

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #### Fonctions secondaires
-
-
+"""
+module pour manipuler les suites de Syracuse
+"""
 # imports
 from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """
+    affichage suite de Syracuse
+    """
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -14,12 +18,11 @@ def syr_plot(lsyr):
                 }
     )
 
-    x = [ i for i in range(len(lsyr)) ]
+    x = list(range(len(lsyr)))
     t = Scatter(x=x, y=lsyr, mode="lines+markers", marker_color = "blue")
     fig.add_trace(t)
     fig.show()
     # fig.write_html('fig.html', include_plotlyjs='cdn')
-    return None
 #######################
 
 def syracuse_l(n):
@@ -31,9 +34,14 @@ def syracuse_l(n):
     Returns:
         list: la suite de Syracuse de source n
     """
-
-    # votre code ici 
-    l = [ ]
+    l = [n]
+    while n!=1:
+        if n%2 == 0: # n paire
+            n = n//2
+            l.append(n)
+        else: # n impaire
+            n = n*3 +1
+            l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,10 +53,8 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
-    # votre code ici
 
-    n = 0
+    n = len(l)
     return n
 
 def temps_de_vol_en_altitude(l):
@@ -61,10 +67,11 @@ def temps_de_vol_en_altitude(l):
         int: le temps de vol en altitude
     """
 
-    # votre code ici
-
-    n = 0
-    return n
+    premier_element = l[0]
+    for indice in range(1,len(l)):
+        if l[indice] < premier_element:
+            return indice
+    return -1
 
 
 def altitude_maximale(l):
@@ -76,25 +83,34 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
-    # votre code ici
-    
-    n = 0
-    return n
 
+    n = max(l)
+    return n
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """
+    appels des fonctions secondaires
+    """
     lsyr = syracuse_l(15)
     syr_plot(lsyr)
     print(temps_de_vol(lsyr))
     print(temps_de_vol_en_altitude(lsyr))
     print(altitude_maximale(lsyr))
 
+    #A = syracuse_l(5)
+    #syr_plot(A)
+    #print(temps_de_vol(A))
+    #print(temps_de_vol_en_altitude(A))
+    #print(altitude_maximale(A))
+
+    #B = syracuse_l(10)
+    #syr_plot(B)
+    #print(temps_de_vol(B))
+    #print(temps_de_vol_en_altitude(B))
+    #print(altitude_maximale(B))
 
 if __name__ == "__main__":
     main()
